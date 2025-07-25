5c53978caa8f213fd60462c801b9b4d8e44d1e77
Eddu <azabacheeduardo85@gmail.com>
2025-07-24 19:53:06 -0500

    feat: load configuration for auth, database, and API in AppModule; set API port from config in main

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {ConfigModule} from '@nestjs/config';
-
+import authConfig from './config/auth.config';
+import databaseConfig from './config/database.config';
+import apiConfig from './config/api.config';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.development.local', '.env'],
+      load: [authConfig, databaseConfig, apiConfig],
     }),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import {ConfigService} from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(configService.get<number>('api.port', 3000));
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
