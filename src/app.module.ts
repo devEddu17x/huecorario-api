efdc7713475da91f1a5b35e2625b84388ed352f1
Eddu <azabacheeduardo85@gmail.com>
2025-07-24 20:15:22 -0500

    feat: add Mongoose integration for database connection in AppModule; update package dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
-import {ConfigModule} from '@nestjs/config';
+import {ConfigModule, ConfigService} from '@nestjs/config';
 import authConfig from './config/auth.config';
 import databaseConfig from './config/database.config';
 import apiConfig from './config/api.config';
+import {MongooseModule} from '@nestjs/mongoose';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.development.local', '.env'],
       load: [authConfig, databaseConfig, apiConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+        dbName: configService.get<string>('database.dbName'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
