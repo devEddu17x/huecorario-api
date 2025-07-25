bbfcd4cc26f9a5d6f5f0e207547ae8c33d08e882
Eddu <azabacheeduardo85@gmail.com>
2025-07-25 12:04:10 -0500

    feat: add CacheModule and CacheService for caching functionality

---
+++ src/cache/cache.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@nestjs/common';
+import {ConfigService} from '@nestjs/config';
+import {Redis} from '@upstash/redis';
+@Injectable()
+export class CacheService {
+  private cacheService: Redis;
+  constructor(private readonly configService: ConfigService) {
+    this.cacheService = new Redis({
+      url: this.configService.get<string>('cache.url'),
+      token: this.configService.get<string>('cache.token'),
+    });
+  }
+
+  async setData<T>(key: string, value: T, ttl?: number): Promise<void> {
+    await this.cacheService.set(key, value, {ex: ttl});
+  }
+
+  async getData<T>(key: string): Promise<T | null> {
+    const data: string = await this.cacheService.get(key);
+    return data ? JSON.parse(data) : null;
+  }
+
+  async deleteData(key: string): Promise<void> {
+    await this.cacheService.del(key);
+  }
+}
