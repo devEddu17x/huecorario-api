f0252e17665512be97759ed8113b2a9b208a68fd
Eddu <azabacheeduardo85@gmail.com>
2025-07-28 18:35:54 -0500

    feat: add cookie-parser middleware for enhanced cookie handling in the application

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-
+import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const configService = app.get(ConfigService);
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('api.port', 3000));
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
