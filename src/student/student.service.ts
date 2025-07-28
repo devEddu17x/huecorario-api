b2fbe2474f5293f39450f34b3246b18ed04a61f1
Eddu <azabacheeduardo85@gmail.com>
2025-07-28 18:35:40 -0500

    feat: enhance StudentService with findByEmail and validateStudent methods for improved student authentication

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,7 +1,8 @@
 import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Student } from './schemas/student.schema';
+import { Student, StudentDocument } from './schemas/student.schema';
+import * as bcrypt from 'bcrypt';
 import { CreateStudentDTO } from './dtos/create-student.dto';
 
 @Injectable()
@@ -18,5 +19,20 @@ export class StudentService {
   async emailIsAvailable(email: string): Promise<boolean> {
     const student = await this.studentModel.findOne({ email });
     return !student;
+  }
+
+  async findByEmail(email: string): Promise<StudentDocument | null> {
+    return await this.studentModel.findOne({ email });
+  }
+
+  async validateStudent(
+    email: string,
+    password: string,
+  ): Promise<StudentDocument | null> {
+    const student = await this.findByEmail(email);
+    if (student && (await bcrypt.compare(password, student.password))) {
+      return student;
+    }
+    return null;
   }
 }
