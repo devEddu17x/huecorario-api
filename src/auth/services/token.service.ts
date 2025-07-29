d400887cc8af34a62eae82016d5adeff26d38377
Eddu <azabacheeduardo85@gmail.com>
2025-07-28 22:27:32 -0500

    feat: enhance TokenService to include cache and config services for token management

--- src/auth/services/token.service.ts
+++ src/auth/services/token.service.ts
@@ -1,6 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Payload } from '../interfaces/payload.interface';
+import { Token } from '../enums/tokens-name.enum';
+import { parseTimeString } from 'src/common/utils/parseTimeString';
+import { CacheService } from 'src/cache/cache.service';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class TokenService {
   constructor(
@@ -8,6 +12,8 @@ export class TokenService {
     private readonly accessJwtService: JwtService,
     @Inject('REFRESH_JWT_SERVICE')
     private readonly refreshJwtService: JwtService,
+    private readonly cacheService: CacheService,
+    private readonly configService: ConfigService,
   ) {
     if (!this.accessJwtService || !this.refreshJwtService) {
       throw new Error('JWT services not properly configured');
@@ -19,7 +25,12 @@ export class TokenService {
       this.accessJwtService.signAsync(payload),
       this.refreshJwtService.signAsync(payload),
     ]);
-
+    await this.cacheService.setData<string>(
+      `${Token.REFRESH_TOKEN}:${payload.email}`,
+      refreshToken,
+      parseTimeString(this.configService.get<string>('jwt.refreshExpiresIn')) /
+        1000,
+    );
     return { accessToken, refreshToken };
   }
 }
