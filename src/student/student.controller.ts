dcbfc2d27a4cad727c25bddbbf5bad0b54a32111
Eddu <azabacheeduardo85@gmail.com>
2025-07-28 22:53:07 -0500

    feat: add StudentController with profile retrieval endpoint

---
+++ src/student/student.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { CustomRequest } from 'src/auth/interfaces/custom-request.interface';
+import { StudentService } from './student.service';
+
+@Controller('student')
+export class StudentController {
+  constructor(private readonly studentService: StudentService) {}
+  @Get('me')
+  getProfile(@Req() req: CustomRequest) {
+    return this.studentService.findByEmail(req.user.email);
+  }
+}
