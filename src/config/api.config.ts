8bd080d5ccef3cb570124551b08352f014ba117d
Eddu <azabacheeduardo85@gmail.com>
2025-08-01 08:31:32 -0500

    fix: update environment variable reference from API_PORT to PORT in api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,8 +1,8 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('api', () => {
-  const { API_PORT } = process.env;
+  const { PORT } = process.env;
   return {
-    port: API_PORT,
+    port: PORT,
   };
 });
