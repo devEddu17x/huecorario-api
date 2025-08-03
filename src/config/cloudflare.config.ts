b038325533d9f69c69a66de10ccf81990e52508e
Eddu <azabacheeduardo85@gmail.com>
2025-08-03 13:57:02 -0500

    feat(config): add BASE_URL to Cloudflare configuration

--- src/config/cloudflare.config.ts
+++ src/config/cloudflare.config.ts
@@ -1,12 +1,18 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('cloudflare', () => {
-  const { ACCOUNT_ID, ACCESS_KEY_ID, SECRET_ACCESS_KEY, BUCKET_NAME } =
-    process.env;
+  const {
+    ACCOUNT_ID,
+    ACCESS_KEY_ID,
+    SECRET_ACCESS_KEY,
+    BUCKET_NAME,
+    BASE_URL,
+  } = process.env;
   return {
     accountId: ACCOUNT_ID,
     accessKeyId: ACCESS_KEY_ID,
     secretAccessKey: SECRET_ACCESS_KEY,
     bucket: BUCKET_NAME,
+    url: BASE_URL,
   };
 });
