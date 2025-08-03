c1ee48c4427140a06e5e6a299a798c743c5aa682
Eddu <azabacheeduardo85@gmail.com>
2025-08-02 19:36:26 -0500

    fix: enhance getSchedulesForCourses method with error handling and response structure

--- src/schedule/schedule.controller.ts
+++ src/schedule/schedule.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  NotFoundException,
+  Post,
+} from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 import { GetSchedulesDTO } from './dtos/get-schedules.dto';
 
@@ -6,10 +12,18 @@ import { GetSchedulesDTO } from './dtos/get-schedules.dto';
 export class ScheduleController {
   constructor(private readonly scheduleService: ScheduleService) {}
 
+  @HttpCode(200)
   @Post()
   async getSchedulesForCourses(@Body() getSchedulesDTO: GetSchedulesDTO) {
-    return this.scheduleService.findByCoursesIdGroupedWithAggregation(
-      getSchedulesDTO.coursesId,
-    );
+    const schedules =
+      await this.scheduleService.findByCoursesIdGroupedWithAggregation(
+        getSchedulesDTO.coursesId,
+      );
+    if (!schedules || Object.keys(schedules).length === 0) {
+      throw new NotFoundException(
+        'Schedules not found for the provided courses.',
+      );
+    }
+    return schedules;
   }
 }
