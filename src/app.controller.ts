f24e683166c09ef8cf239341bb30e5c5ee54b4fd
Eddu <azabacheeduardo85@gmail.com>
2025-08-05 11:52:34 -0500

    feat(controller): add NoAccesTokenNeeded decorator to health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { NoAccesTokenNeeded } from './common/decorators/is-public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @NoAccesTokenNeeded()
   @Get('health')
   getHello(): string {
     return this.appService.getHealth();
