325088e183aceba24d0eae325880ea947ce5f536
Eddu <azabacheeduardo85@gmail.com>
2025-08-05 02:48:17 -0500

    feat(logging): add morgan middleware for HTTP request logging

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   app.use(cookieParser());
+  app.use(morgan('dev'));
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('api.port', 3000));
   console.log(`Application is running on: ${await app.getUrl()}`);
